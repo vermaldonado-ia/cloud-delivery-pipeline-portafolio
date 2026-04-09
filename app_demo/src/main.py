495368c0c25dc3574350356e98de17abc87c142a
Verónica Maldonado Project Manager | Agil | PMP <127198190+vermaldonado-ia@users.noreply.github.com>
2026-04-09 14:02:18 -0400

    Update main.py

--- app_demo/src/main.py
+++ app_demo/src/main.py
@@ -1,13 +1,17 @@
 def suma(a, b):
     return a + b
 
+
 def resta(a, b):
     return a - b
 
+
 def multiplicacion(a, b):
     return a * b
+
 
 def division(a, b):
     if b == 0:
         raise ValueError("No se puede dividir por cero")
     return a / b
+    
